5e9e494fa0c1eeeca2292b3022a28542768c666d
psciem-2008 <p.sciem@gmail.com>
2026-09-18 06:54:22 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const SCIEM_CONFIG = Object.freeze({
+
+  API: {
+    CONTENT:
+      "https://script.google.com/macros/s/AKfycbx6sEgvFD41nzq08RUAn6lEht4EwtMMdWeRwtohtlqIgDCNM9uCkYjD1AqRhL08lVZb/exec",
+
+    SERVICES:
+      "https://script.google.com/macros/s/AKfycby63uAqfVHCZCy5168Z1_X9WgYljCiLM19a3UyXdVIN9A3OYp4LCDhmfi2vETZIFuCitQ/exec"
+  },
+
+  TIMEOUT: 10000,
+
+  CACHE_TIME: 5 * 60 * 1000,
+
+  DEBUG: true
+
+});
