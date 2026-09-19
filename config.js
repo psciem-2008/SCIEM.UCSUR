8651612da7b0d2ccaa7184199c65adb77d1c2e2f
psciem-2008 <p.sciem@gmail.com>
2026-09-18 20:11:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const SCIEM_CONFIG = Object.freeze({
 
   API: {
-    CONTENT:
+
+    SCRIPT_A:
       "https://script.google.com/macros/s/AKfycbx6sEgvFD41nzq08RUAn6lEht4EwtMMdWeRwtohtlqIgDCNM9uCkYjD1AqRhL08lVZb/exec",
 
-    SERVICES:
+    SCRIPT_B:
       "https://script.google.com/macros/s/AKfycby63uAqfVHCZCy5168Z1_X9WgYljCiLM19a3UyXdVIN9A3OYp4LCDhmfi2vETZIFuCitQ/exec"
+
   },
 
-  TIMEOUT: 10000,
-
-  CACHE_TIME: 5 * 60 * 1000,
+  TIMEOUT: 12000,
 
   DEBUG: true
 
